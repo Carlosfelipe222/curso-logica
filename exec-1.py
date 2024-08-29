edff623cef245405a7bd5a0e782faa09838a8d97
Carlosfelipe222 <cf109826@gmail.com>
2024-08-29 19:38:37 -0300

    Update exec-1.py

--- exec-1.py
+++ exec-1.py
@@ -1,7 +1,7 @@
 '''
 
 1. Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".
-1. Frase na tela - Implemente um programa que escreve na tela a frase "bla bla bla".
+
 
 '''
 
